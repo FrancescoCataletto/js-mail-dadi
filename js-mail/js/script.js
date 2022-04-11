b649dc65610215e0b0371d1d24eff168d1a5703f
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-11 16:53:01 +0200

    first commit

---
+++ js-mail/js/script.js
@@ -0,0 +1,6 @@
+/*
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere (creare quindi una lista di utenti autorizzati),
+stampa un messaggio appropriato sull’esito del controllo.
+
+*/
