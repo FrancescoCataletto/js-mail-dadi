96e25e317cbf8180c02f9780b910b039a143437e
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-11 17:30:19 +0200

    set variables to generate random numbers

--- js-dadi/js/script.js
+++ js-dadi/js/script.js
@@ -3,3 +3,9 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 */
+
+const playerRandomNumber = Math.floor(Math.random()* 6) + 1;
+
+const computerRandomNumber = Math.floor(Math.random()* 6) + 1;
+
+console.log(playerRandomNumber, computerRandomNumber);
